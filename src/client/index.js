8a8e298fa8d888d6b797084000c7b10957b2eec1
Nathan Holman <nathan.m.holman@gmail.com>
2021-02-05 12:57:27 -0500

    Update submit and return buttons

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,14 @@
 import { postData } from './js/post'
 import { handleSubmit } from './js/formHandler'
+import { updateLayout } from './js/formHandler'
 
 import './styles/resets.scss'
 import './styles/base.scss'
-import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
 export {
     postData,
-    handleSubmit
+    handleSubmit,
+    updateLayout
 }
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,10 +1,20 @@
-function showResults() {
-    document.getElementById('submission').value = '';
-    document.getElementById('results').style.visibility = 'visible';
-    document.getElementById('form').style.height = '25px';
-    document.getElementById('submission').style.display = 'none';
-    document.getElementById('button').style.display = 'none';
+function updateLayout() {
+    if (document.getElementById('results').style.visibility == 'hidden' || document.getElementById('results').style.visibility == '') {
+        console.log('This should show results');
 
+        document.getElementById('submission').value = '';
+        document.getElementById('results').style.visibility = 'visible';
+        document.getElementById('form').style.cssText = 'padding:0px 0px; border: none;';
+        document.getElementById('submission').style.display = 'none';
+        document.getElementById('button').style.display = 'none';
+        document.getElementById('return').style.display = 'block';
+    } else {
+        document.getElementById('results').style.visibility = 'hidden';
+        document.getElementById('form').style.cssText = '';
+        document.getElementById('submission').style.display = 'block';
+        document.getElementById('button').style.display = 'block';
+        document.getElementById('return').style.display = 'none';
+    }
 }
 
 
@@ -49,11 +59,12 @@ function handleSubmit(event) {
 
             document.getElementById('submittedText').innerHTML = `<strong>${fullText}</strong>`;
 
-            showResults();
+            updateLayout();
             console.log(res);
         })
 }
 
 export {
-    handleSubmit
+    handleSubmit,
+    updateLayout
 }
